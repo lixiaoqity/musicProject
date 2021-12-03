b3bfdfc95229556413e08646ca6a15b001e009bd
Xiaoqi Li <59097971+lixiaoqity@users.noreply.github.com>
2021-12-03 17:18:24 -0500

    Add more files

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment6.Startup))]
+
+namespace Assignment6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
